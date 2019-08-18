bd25fbf4980d29e810724136a706ebdf8d8832a7
Amrit <amrittheengineer@users.noreply.github.com>
2019-08-18 21:08:54 +0530

    Login modal added in Events and individual event pages. Comments are removed

--- js/main.js
+++ js/main.js
@@ -1,41 +1,6 @@
-function turnOn() {
-  $("div#centerDiv")
-    .css({ display: "block" })
-    .animate(
-      {
-        width: "100%",
-        left: "0%"
-      },
-      300,
-      () => {
-        $("div#topDiv").animate(
-          {
-            //51% for chrome
-            height: "100%",
-            opacity: 0
-          },
-          800
-        );
-        $("div#bottomDiv").animate(
-          {
-            //51% for chrome
-            height: "100%",
-            opacity: 0
-          },
-          800,
-          () => {
-            $("div#centerDiv").hide();
-            setTimeout(turnOff, 3000);
-          }
-        );
-      }
-    );
-}
-
 function turnOff() {
   $("div#topDiv").animate(
     {
-      //51% for chrome
       height: "50%",
       opacity: 1
     },
@@ -43,7 +8,6 @@ function turnOff() {
   );
   $("div#bottomDiv").animate(
     {
-      //51% for chrome
       height: "50%",
       opacity: 1
     },
@@ -58,7 +22,7 @@ function turnOff() {
           },
           300,
           () => {
-            setTimeout(turnOn, 3000);
+            window.open("/events.html", "_self");
           }
         );
     }
@@ -77,6 +41,7 @@ function loaderDot() {
     $("#txt").text($("#txt").text() + ".");
     $("#txt").attr("data-text", String($("#txt").attr("data-text")) + ".");
   }
+  setTimeout(turnOff, 1000);
 }
 
 $(function() {
@@ -132,117 +97,4 @@ $(document).ready(() => {
     }, 3000);
   });
 });
-particleConfig = {
-  particles: {
-    number: {
-      value: 100,
-      density: {
-        enable: true,
-        value_area: 800
-      }
-    },
-    color: {
-      value: "#0a557b"
-    },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#000000"
-      },
-      polygon: {
-        nb_sides: 5
-      }
-    },
-    opacity: {
-      value: 0.5,
-      random: false,
-      anim: {
-        enable: false,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false
-      }
-    },
-    size: {
-      value: 3,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 40,
-        size_min: 0.1,
-        sync: false
-      }
-    },
-    line_linked: {
-      enable: true,
-      distance: 150,
-      color: "#082640",
-      opacity: 0.4,
-      width: 1
-    },
-    move: {
-      enable: true,
-      speed: 6,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200
-      }
-    }
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: false,
-        mode: "grab"
-      },
-      onclick: {
-        enable: false,
-        mode: "push"
-      },
-      resize: true
-    },
-    modes: {
-      grab: {
-        distance: 140,
-        line_linked: {
-          opacity: 1
-        }
-      },
-      bubble: {
-        distance: 400,
-        size: 40,
-        duration: 2,
-        opacity: 8,
-        speed: 3
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4
-      },
-      push: {
-        particles_nb: 4
-      },
-      remove: {
-        particles_nb: 2
-      }
-    }
-  },
-  retina_detect: true
-};
 particlesJS.load("particle-bg", "js/particles-config.json");
-// $('#login-btn').click();
-
-// document.querySelectorAll('input').forEach(inp => {
-//   inp.onfocus = e => {
-//     console.log(e);
-
-//     e.preventDefault();}
-// })
